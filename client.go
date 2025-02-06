29926fef68b04e4a58dbe5bcdc267284c8157b0d
copper friddle <friddle@qq.com>
2025-02-06 21:05:40 +0800

    Update client.go
    添加deepseek模型。临时解决下

--- client.go
+++ client.go
@@ -134,7 +134,7 @@ func (c *client) Ask(cfg *AskConfig) (answer []byte, err error) {
 		openai.ModelGPT_4, openai.ModelGPT_4_0314,
 		openai.ModelGPT_4_32K, openai.ModelGPT_4_32K_0314,
 		openai.ModelGPT_4_Turbo, openai.ModelGPT_4_1106_Preview,
-		openai.ModelQwenMax:
+		openai.ModelQwenMax,"deepseek-reasoner","deepseek-chat":
 		// chat
 		currentMessageLength := 0
 		messages := []openai.CreateChatCompletionMessage{}
